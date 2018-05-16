5f6e6b2a373f7e84d4bff88efd505b3568e121a4
hanif <hanif_1362@yahoo.com>
2018-05-16 22:03:02 +0200

    Add layouts: header, footer, list, detail, search Adding Models Enable very basic version of login

--- db/migrate/20180516195609_create_users.rb
+++ db/migrate/20180516195609_create_users.rb
@@ -1,12 +1,16 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :users do |t|
-      t.string :username
-      t.string :name
-      t.string :family
-      t.string :email
+    #create_table :users do |t|
+    #  t.string :username
+    #  t.string :name
+    #  t.string :family
+    #  t.string :email#
 
-      t.timestamps
-    end
+      #t.timestamps
+    #end
+    add_column :users, :family, :string
+    add_column :users, :username, :string
+    add_column :users, :password, :string
+
   end
 end
