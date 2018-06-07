0a3afd1b72074e5742e9f77a1ad06026ce508ae7
hanif <hanif_1362@yahoo.com>
2018-06-07 16:10:20 +0200

    Adding Signup view and action Adding logout functionality Adding Styling(bootstrap) to the person details page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if($current_user.nil?)
+      redirect_to action: 'login'
+    end
+    puts $current_user
     @user = $current_user
   end
 
@@ -28,8 +32,20 @@ class StaticPagesController < ApplicationController
     redirect_to action: 'home'
   end
 
+  def signup
+    @user = User.new
+    render :layout => nil
+  end
+
+  def logout
+    $current_user = nil
+    puts "Set to nil #{$current_user}"
+    redirect_to action: "login"
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :username)
   end
+
 end
